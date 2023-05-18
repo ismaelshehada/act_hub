a1b48c3c0d9a3cf1c6ba0b552142398709cb93cf
ismaelshehada <s-b-r2011@live.com>
2023-05-18 12:49:04 +0300

    prepare assets file

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -1,5 +1,6 @@
 const String imagePath = "assets/images";
 
 class ManagerAssets {
-  static const String logo = "";
+  static const String logo = "$imagePath/logo.svg";
+  static const String background = "$imagePath/background.png";
 }
