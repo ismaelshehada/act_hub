ca84de8e250fb933b80293531033c02206a3cd69
ismaelshehada <s-b-r2011@live.com>
2023-05-18 13:34:59 +0300

    create manager_sizes.dart file

---
+++ lib/core/resources/manager_sizes.dart
@@ -0,0 +1,28 @@
+import 'package:act_hub/core/util/size_util.dart';
+
+class ManagerHeight {
+  static double h2 = SizeUtil.setHeight(2);
+  static double h4 = SizeUtil.setHeight(4);
+  static double h6 = SizeUtil.setHeight(6);
+  static double h8 = SizeUtil.setHeight(8);
+  static double h10 = SizeUtil.setHeight(10);
+  static double h12 = SizeUtil.setHeight(12);
+}
+
+class ManagerWidth {
+  static double w2 = SizeUtil.setWidth(2);
+  static double w4 = SizeUtil.setWidth(4);
+  static double w6 = SizeUtil.setWidth(6);
+  static double w8 = SizeUtil.setWidth(8);
+  static double w10 = SizeUtil.setWidth(10);
+  static double w12 = SizeUtil.setWidth(12);
+}
+
+class ManagerRadius {
+  static double r2 = SizeUtil.setRadius(2);
+  static double r4 = SizeUtil.setRadius(4);
+  static double r6 = SizeUtil.setRadius(6);
+  static double r8 = SizeUtil.setRadius(8);
+  static double r10 = SizeUtil.setRadius(10);
+  static double r12 = SizeUtil.setRadius(12);
+}
