971844fe705cca44cb341fac77fce23eb7e284d8
ismaelshehada <s-b-r2011@live.com>
2023-05-18 15:15:50 +0300

    create theme

---
+++ lib/core/theme/manager_light_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import '../resources/manager_colors.dart';
+
+ThemeData managerLightThemeData() {
+  return ThemeData.light()
+      .copyWith(useMaterial3: true, primaryColor: ManagerColors.white);
+}
