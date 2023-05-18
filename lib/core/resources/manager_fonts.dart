ce227b7e95e63952dcfb1ef8215f709badcb0dbc
ismaelshehada <s-b-r2011@live.com>
2023-05-18 13:47:26 +0300

    create manager_fonts.dart file

---
+++ lib/core/resources/manager_fonts.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:act_hub/core/util/size_util.dart';
+
+class ManagerFontsFamily {
+  static const String fontFamily = 'Poppins';
+}
+
+class ManagerFontWight {
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+}
+
+class ManagerFontSize {
+  static double s2 = SizeUtil.setFontSize(2);
+  static double s4 = SizeUtil.setFontSize(4);
+  static double s6 = SizeUtil.setFontSize(6);
+  static double s8 = SizeUtil.setFontSize(8);
+  static double s10 = SizeUtil.setFontSize(10);
+}
