711d7de21d773149b74195d26119de6e07a884b6
ismaelshehada <s-b-r2011@live.com>
2023-05-21 12:32:08 +0300

    build splash_view.dart in progress

--- lib/features/splash/presentation/view/splash_view.dart
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -1,3 +1,4 @@
+import 'package:act_hub/core/resources/manager_assets.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
@@ -5,6 +6,15 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(ManagerAssets.background),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
   }
 }
