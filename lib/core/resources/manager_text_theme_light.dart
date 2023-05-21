1c68fe2f153d5d6e4476f04e9323ee22874f846d
ismaelshehada <s-b-r2011@live.com>
2023-05-21 15:50:55 +0300

    prepare theme data

--- lib/core/resources/manager_text_theme_light.dart
+++ lib/core/resources/manager_text_theme_light.dart
@@ -1,3 +1,40 @@
+
+
+import 'package:act_hub/core/resources/manager_colors.dart';
+import 'package:act_hub/core/resources/manager_fonts.dart';
+import 'package:act_hub/core/resources/manager_styles.dart';
 import 'package:flutter/material.dart';
 
-class ManagerTextThemeLight extends TextTheme {}
+class ManagerTextThemeLight extends TextTheme {
+
+
+  @override
+  TextStyle get displayMedium => getMediumTextStyle(
+        fontSize: ManagerFontSize.s20,
+        color: ManagerColors.textColor,
+      );
+
+  @override
+  TextStyle get displaySmall => getBoldTextStyle(
+        fontSize: ManagerFontSize.s16,
+        color: ManagerColors.textColor,
+      );
+
+  @override
+  TextStyle get headlineMedium => getMediumTextStyle(
+    fontSize: ManagerFontSize.s16,
+    color: ManagerColors.textColor,
+  );
+
+  @override
+  TextStyle get titleMedium => getMediumTextStyle(
+    fontSize: ManagerFontSize.s14,
+    color: ManagerColors.textColor,
+  );
+
+  @override
+  TextStyle get boldLargeTitle => getMediumTextStyle(
+    fontSize: ManagerFontSize.s16,
+    color: ManagerColors.textColor,
+  );
+}
