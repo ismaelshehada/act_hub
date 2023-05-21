11721e5e7bfac28c252ceaac18433cb636e0c604
ismaelshehada <s-b-r2011@live.com>
2023-05-21 12:48:13 +0300

    add logo to splash_view.dart

--- lib/features/splash/presentation/view/splash_view.dart
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -1,5 +1,7 @@
 import 'package:act_hub/core/resources/manager_assets.dart';
+import 'package:act_hub/core/resources/manager_sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({Key? key}) : super(key: key);
@@ -14,6 +16,7 @@ class SplashView extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
+        child: Center(child: SvgPicture.asset(ManagerAssets.logo,width: ManagerWidth.w166,)),
       ),
     );
   }
