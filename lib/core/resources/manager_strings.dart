12be758f4f4fa29c45b06ad74b1b076fa653916b
ismaelshehada <s-b-r2011@live.com>
2023-05-24 23:42:34 +0300

    add outBoarding strings

--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -2,4 +2,13 @@ class ManagerStrings {
   static const String acthub = 'Acthub';
   static const String noRoutFound = 'page not Found';
   static const String skip = 'Skip';
+  static const String outBoardingTitle1 = 'Find your cozy working space';
+  static const String outBoardingTitle2 = 'Find your cozy Courses';
+  static const String outBoardingTitle3 = 'Find your recording studio';
+  static const String outBoardingSubTitle1 =
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.';
+  static const String outBoardingSubTitle2 =
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.';
+  static const String outBoardingSubTitle3 =
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.';
 }
