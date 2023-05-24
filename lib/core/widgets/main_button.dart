10eddf62a56025f409c2fbad0a3967c611cc880a
ismaelshehada <s-b-r2011@live.com>
2023-05-24 23:40:39 +0300

    prepare main button widget

--- lib/core/widgets/main_button.dart
+++ lib/core/widgets/main_button.dart
@@ -1,23 +1,28 @@
+import 'package:act_hub/config/constants.dart';
 import 'package:act_hub/core/resources/manager_colors.dart';
-import 'package:act_hub/core/resources/manager_fonts.dart';
-import 'package:act_hub/core/resources/manager_strings.dart';
-import 'package:act_hub/core/resources/manager_styles.dart';
+import 'package:act_hub/core/resources/manager_sizes.dart';
 import 'package:flutter/material.dart';
 
-class mainButton extends StatelessWidget {
-  const mainButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: () {},
-      child: Text(
-        ManagerStrings.skip,
-        style: getRegularTextStyle(
-            fontSize: ManagerFontSize.s16, color: ManagerColors.textColor),
-      ),
-    );
-  }
+Widget MainButton({
+  required Widget child,
+  void Function()? onPress,
+  ShapeBorder? shapeBorder,
+  Color? color,
+  double? minWidth,
+  double? height,
+  double? elevation,
+}) {
+  return MaterialButton(
+    onPressed: onPress ?? () {},
+    shape: shapeBorder ??
+        RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+          ManagerRadius.r12,
+        )),
+    color: color ?? ManagerColors.transparent,
+    minWidth: minWidth ?? ManagerWidth.w16,
+    height: height ?? ManagerHeight.h16,
+    elevation: elevation ?? Constants.elevationButton,
+    child: child,
+  );
 }
