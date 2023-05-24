82f03589b4d0f128888921b77355e4762d0bb57f
ismaelshehada <s-b-r2011@live.com>
2023-05-24 23:45:25 +0300

    prepare out_boarding_view.dart

--- lib/features/out_boarding/presentation/view/out_boarding_view.dart
+++ lib/features/out_boarding/presentation/view/out_boarding_view.dart
@@ -1,10 +1,14 @@
-import 'package:act_hub/core/resources/manager_assets.dart';
+import 'package:act_hub/core/resources/manager_colors.dart';
+import 'package:act_hub/core/resources/manager_fonts.dart';
 
 import 'package:act_hub/core/resources/manager_sizes.dart';
+import 'package:act_hub/core/resources/manager_strings.dart';
+import 'package:act_hub/core/resources/manager_styles.dart';
 
 import 'package:act_hub/core/widgets/main_button.dart';
+import 'package:act_hub/features/out_boarding/presentation/controller/out_boarding_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 class OutBoardingView extends StatelessWidget {
   const OutBoardingView({Key? key}) : super(key: key);
@@ -17,22 +21,87 @@ class OutBoardingView extends StatelessWidget {
         child: Container(
           margin: EdgeInsets.symmetric(
               horizontal: ManagerWidth.w16, vertical: ManagerWidth.w20),
-          child: Column(
-            children: [
-              const Align(
-                alignment: AlignmentDirectional.topEnd,
-                child: mainButton(),
-              ),
-              SizedBox(
-                height: ManagerHeight.h86,
-              ),
-              SvgPicture.asset(
-                ManagerAssets.outBoardingIllustration1,
-                width: double.infinity,
-                height: ManagerHeight.h206,
-              )
-            ],
-          ),
+          child: GetBuilder<OutBoardingController>(builder: (controller) {
+            return Column(
+              children: [
+                Visibility(
+                  visible: !controller.isLastPage(),
+                  maintainSize: true,
+                  maintainAnimation: true,
+                  maintainState: true,
+                  child: Align(
+                    alignment: AlignmentDirectional.topEnd,
+                    child: MainButton(
+                      onPress: () {
+                        controller.skipPage();
+                      },
+                      child: Text(
+                        ManagerStrings.skip,
+                        style: getRegularTextStyle(
+                            fontSize: ManagerFontSize.s16,
+                            color: ManagerColors.textColor),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                    child: PageView(
+                  controller: controller.pageController,
+                  children: [
+                    ...controller.pageViewItem,
+                  ],
+                  onPageChanged: (index) {
+                    controller.setCurrentPage(index);
+                  },
+                )),
+                Visibility(
+                  visible: !controller.isLastPage(),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Visibility(
+                        visible: controller.showBackButton(),
+                        child: MainButton(
+                            onPress: () {
+                              controller.nextPage();
+                            },
+                            child: const Icon(
+                              Icons.arrow_back_outlined,
+                              color: ManagerColors.iconColor,
+                            ),
+                            shapeBorder: const CircleBorder(),
+                            minWidth: ManagerWidth.w50,
+                            height: ManagerHeight.h50,
+                            color: ManagerColors.primaryColor),
+                      ),
+                      MainButton(
+                          onPress: () {
+                            controller.nextPage();
+                          },
+                          child: const Icon(
+                            Icons.arrow_forward_outlined,
+                            color: ManagerColors.iconColor,
+                          ),
+                          shapeBorder: const CircleBorder(),
+                          minWidth: ManagerWidth.w50,
+                          height: ManagerHeight.h50,
+                          color: ManagerColors.primaryColor),
+                    ],
+                  ),
+                  replacement: MainButton(
+                      child: Text(
+                        ManagerStrings.skip,
+                        style: getRegularTextStyle(
+                            fontSize: ManagerFontSize.s14,
+                            color: ManagerColors.textColor),
+                      ),
+                      minWidth: double.infinity,
+                      height: ManagerHeight.h40,
+                      color: ManagerColors.primaryColor),
+                ),
+              ],
+            );
+          }),
         ),
       ),
     );
