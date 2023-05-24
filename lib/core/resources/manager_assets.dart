858a912ed93c69d9cc317ac8587435e6b8e43184
ismaelshehada <s-b-r2011@live.com>
2023-05-24 23:41:06 +0300

    add path image outBoarding

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -4,4 +4,6 @@ class ManagerAssets {
   static const String logo = "$imagePath/logo.svg";
   static const String background = "$imagePath/background.png";
   static const String outBoardingIllustration1 = "$imagePath/illustration1.svg";
+  static const String outBoardingIllustration2 = "$imagePath/illustration2.svg";
+  static const String outBoardingIllustration3 = "$imagePath/illustration3.svg";
 }
