fc409e0a3a6e3f379f4f341c84a8ee59159ba0b3
ismaelshehada <s-b-r2011@live.com>
2023-05-25 14:29:16 +0300

    add initNodule in main

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
-void main() {
-  initModule();
+void main() async{
+  await initModule();
   runApp(MyApp());
 }
 
