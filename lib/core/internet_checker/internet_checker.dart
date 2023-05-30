dc1649bda1beca29600be0670cec2f0ffefbdf00
ismaelshehada <s-b-r2011@live.com>
2023-05-30 14:01:31 +0300

    edit internet_checker.dart

--- lib/core/internet_checker/internet_checker.dart
+++ lib/core/internet_checker/internet_checker.dart
@@ -1,6 +1,8 @@
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
-abstract class NetworkInfo {}
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
 
 class NetworkInfoImpl extends NetworkInfo {
   final InternetConnectionCheckerPlus _internetConnectionCheckerPlus;
