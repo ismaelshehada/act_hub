e8a65c2525d6a1ff41ac172cf65b98b5e0348882
ismaelshehada <s-b-r2011@live.com>
2023-05-30 14:55:03 +0300

    prepare login_repository_impl.dart

---
+++ lib/features/auth/domin/repository_impl/login_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:act_hub/config/constants.dart';
+import 'package:act_hub/core/error_handler/error_handler.dart';
+import 'package:act_hub/core/internet_checker/internet_checker.dart';
+import 'package:act_hub/features/auth/data/data_source/remote_login_data_source.dart';
+import 'package:act_hub/features/auth/data/mapper/login_mapper.dart';
+import 'package:act_hub/features/auth/data/request/login_request.dart';
+import 'package:act_hub/features/auth/domin/model/login.dart';
+import 'package:act_hub/features/auth/domin/repository/login_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class LoginRepositoryImpl implements LoginRepository {
+  final RemoteLoginDataSourceImplement _dataSourceImplement;
+
+  final NetworkInfoImpl networkInfoImpl;
+
+  LoginRepositoryImpl(this._dataSourceImplement, this.networkInfoImpl);
+
+  @override
+  Future<Either<Failure, Login>> login(LoginRequest loginRequest) async {
+    if (await networkInfoImpl.isConnected) {
+      final response = await _dataSourceImplement.login(loginRequest);
+      return Right(response.toDomain());
+    } else {
+      return Left(
+        Failure(ResponseCode.NO_INTERNET_CONNECTION.value,
+            ApiConstant.no_internet_connection),
+      );
+    }
+  }
+}
