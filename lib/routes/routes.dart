046b458c125fc5be0f5703fd50c3436d74e9ed6d
ismaelshehada <s-b-r2011@live.com>
2023-06-01 13:28:50 +0300

    add initLoginModule to routes.dart

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -22,6 +22,7 @@ class RouteGenerator {
         initOutBoarding();
         return MaterialPageRoute(builder: (_) => const OutBoardingView());
       case Routes.loginView:
+        initLoginModule();
         return MaterialPageRoute(builder: (_) => const LoginView());
       default:
         return unDefinedRoute();
