250d42c74bc63914a87ca0859e31a2d45381858c
ismaelshehada <s-b-r2011@live.com>
2023-06-06 02:02:20 +0300

    edit scaffold_with_background_image.dart

--- lib/core/widgets/scaffold_with_background_image.dart
+++ lib/core/widgets/scaffold_with_background_image.dart
@@ -1,14 +1,39 @@
+import 'package:act_hub/config/constants.dart';
 import 'package:act_hub/core/resources/manager_assets.dart';
 import 'package:act_hub/core/resources/manager_colors.dart';
+import 'package:act_hub/core/widgets/will_pop_scope.dart';
+import 'package:act_hub/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
-Widget scaffoldWithBackgroundImage(
-    {required Widget child,
-    Color backgroundColor = ManagerColors.white,
-    String image = ManagerAssets.background}) {
-  return Scaffold(
-    backgroundColor: backgroundColor,
-    body: Container(
+Widget scaffoldWithBackgroundImage({
+  required Widget child,
+  Color backgroundColor = ManagerColors.primaryColor,
+  String image = ManagerAssets.background,
+  bool isRegisterView = false,
+}) {
+  return willPopScope(
+    child: Scaffold(
+      backgroundColor: backgroundColor,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: ManagerColors.transparent,
+        elevation: Constants.elevation,
+        leading: isRegisterView
+            ? IconButton(
+                onPressed: () {
+                  Get.offAllNamed(Routes.loginView);
+                },
+                icon: const Icon(
+                  Icons.arrow_back_outlined,
+                  color: ManagerColors.white,
+                ),
+              )
+            : Container(),
+      ),
+      extendBodyBehindAppBar: true,
+      body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage(
@@ -17,6 +42,8 @@ Widget scaffoldWithBackgroundImage(
             fit: BoxFit.cover,
           ),
         ),
-        child: child),
+        child: child,
+      ),
+    ),
   );
 }
