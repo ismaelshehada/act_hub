fc575a31f25e7df8323e4d4ecb4c41ca4a31a496
ismaelshehada <s-b-r2011@live.com>
2023-06-06 01:55:22 +0300

    add constant for register

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -8,6 +8,7 @@ class Constants {
   static const int outBoardingDurationTime = 1;
   static const int loginFirstPartFlex = 1;
   static const int loginSecondPartFlex = 3;
+  static const int loginTimer = 1;
 }
 
 class ConstantsPrefsKey {
@@ -15,6 +16,8 @@ class ConstantsPrefsKey {
   static const String token = 'token';
   static const String email = 'email';
   static const String password = 'password';
+  static const String loggedIn = 'logged_in';
+
 }
 
 class ApiConstant {
@@ -24,11 +27,18 @@ class ApiConstant {
   static const int receiveTimeoutDuration = 60;
   static const String email = 'email';
   static const String password = 'password';
+  static const String passwordConfirmation = 'password_confirmation';
+  static const String phone = 'phone';
   static const String message = 'message';
   static const String errors = 'errors';
   static const String error = 'error';
   static const String token = 'token';
   static const String badRequest = 'bad request';
   static const String noInternetConnection = 'No Internet Connection';
+  static const String acceptLanguage = 'Accept-Language';
+  static const String english = 'en';
+  static const String arabic = 'ar';
+  static const String status = 'status';
+  static const String name = 'name';
 }
 
