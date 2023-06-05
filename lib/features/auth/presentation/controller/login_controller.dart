74b6eb634c074d7be4c458491b72b195a6e25eff
ismaelshehada <s-b-r2011@live.com>
2023-06-06 01:57:02 +0300

    edit login_controller.dart

--- lib/features/auth/presentation/controller/login_controller.dart
+++ lib/features/auth/presentation/controller/login_controller.dart
@@ -1,21 +1,26 @@
+import 'package:act_hub/config/constants.dart';
 import 'package:act_hub/config/dependency_injection.dart';
+import 'package:act_hub/core/extensions/extensions.dart';
 import 'package:act_hub/core/resources/manager_colors.dart';
 import 'package:act_hub/core/resources/manager_fonts.dart';
 import 'package:act_hub/core/resources/manager_sizes.dart';
 import 'package:act_hub/core/resources/manager_strings.dart';
 import 'package:act_hub/core/resources/manager_styles.dart';
 import 'package:act_hub/core/state_render/state_renderer.dart';
+import 'package:act_hub/core/storage/local/app_settings_shared_preferences.dart';
 import 'package:act_hub/core/widgets/main_button.dart';
 import 'package:act_hub/features/auth/domin/use_case/login_use_case.dart';
+import 'package:act_hub/routes/routes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
   late TextEditingController email = TextEditingController();
   late TextEditingController password = TextEditingController();
-
   late final LoginUseCase _loginUseCase = instance<LoginUseCase>();
   var formKey = GlobalKey<FormState>();
+  final AppSettingsSharedPreferences _appSettingsSharedPreferences =
+      instance<AppSettingsSharedPreferences>();
 
   Future<void> login(BuildContext context) async {
     dialogRender(
@@ -24,75 +29,74 @@ class LoginController extends GetxController {
         message: ManagerStrings.loading,
         title: '',
         retryAction: () {});
-    (await _loginUseCase.execute(LoginBaseUseCaseInput(
-      email: email.text,
-      password: password.text,
-    ),) )
-        .fold(
-            (l) =>
-        {
-          Get.back(),
-          dialogRender(
-            context: context,
-            stateRenderType: StateRenderType.popUpErrorState,
-            message: l.message,
-            title: '',
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: ManagerWidth.w65,
-              ),
-              child: mainButton(
-                child: Text(
-                  ManagerStrings.ok,
-                  style: getMediumTextStyle(
-                    fontSize: ManagerFontSize.s16,
-                    color: ManagerColors.white,
-                  ),
-                ),
-                onPress: () {
-                  Get.back();
-                },
-                color: ManagerColors.primaryColor,
-                height: ManagerHeight.h40,
+    (await _loginUseCase.execute(
+      LoginBaseUseCaseInput(email: email.text, password: password.text),
+    ))
+        .fold((l) {
+      Get.back();
+      dialogRender(
+        context: context,
+        stateRenderType: StateRenderType.popUpErrorState,
+        message: l.message,
+        title: '',
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: ManagerWidth.w65,
+          ),
+          child: mainButton(
+            child: Text(
+              ManagerStrings.ok,
+              style: getMediumTextStyle(
+                fontSize: ManagerFontSize.s16,
+                color: ManagerColors.white,
               ),
             ),
-            retryAction: () {},
-          )
-        },
-            (r) =>
-        {
-          Get.back(),
-          dialogRender(
-            context: context,
-            stateRenderType: StateRenderType.popUpSuccessState,
-            message: ManagerStrings.thanks,
-            title: '',
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: ManagerWidth.w65,
-              ),
-              child: mainButton(
-                child: Text(
-                  ManagerStrings.ok,
-                  style: getMediumTextStyle(
-                    fontSize: ManagerFontSize.s16,
-                    color: ManagerColors.white,
-                  ),
-                ),
-                onPress: () {
-                  Get.back();
-                },
-                color: ManagerColors.primaryColor,
-                height: ManagerHeight.h40,
+            onPress: () {
+              Get.back();
+            },
+            color: ManagerColors.primaryColor,
+            height: ManagerHeight.h40,
+          ),
+        ),
+        retryAction: () {},
+      );
+    }, (r) {
+      _appSettingsSharedPreferences.setEmail(email.text);
+      _appSettingsSharedPreferences.setPassword(password.text);
+      _appSettingsSharedPreferences.setToken(r.token.onNull());
+      Get.back();
+      dialogRender(
+        context: context,
+        stateRenderType: StateRenderType.popUpSuccessState,
+        message: ManagerStrings.thanks,
+        title: '',
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: ManagerWidth.w65,
+          ),
+          child: mainButton(
+            child: Text(
+              ManagerStrings.ok,
+              style: getMediumTextStyle(
+                fontSize: ManagerFontSize.s16,
+                color: ManagerColors.white,
               ),
             ),
-            retryAction: () {},
-          )
-        });
-  }
-
-  @override
-  void onClose() {
-    email.dispose();
+            onPress: () {
+              Get.back();
+            },
+            color: ManagerColors.primaryColor,
+            height: ManagerHeight.h40,
+          ),
+        ),
+        retryAction: () {},
+      );
+      Future.delayed(
+          const Duration(
+            seconds: Constants.loginTimer,
+          ), () {
+        Get.offAllNamed(Routes.homeView);
+      });
+    });
   }
 }
